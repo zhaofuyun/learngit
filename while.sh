77fcf1db541b783733de5baf63acf972d15b54ee
ken <1751728214@qq.com>
2016-03-01 03:08:16 +0800

    conflict fixed

--- while.sh
+++
@@ -1,8 +0,0 @@
-     #!/bin/bash
-#使用while每隔3秒输出时间
-     while :
-     do
-          date +%T
-          sleep 3
-     done
-
--- while4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#使用while循环检测输入内容是否为数字
-n=1
-     while [ ! -z "$n" ] 
-     do
-          read -p  '请输入一个数字： ' m
-          n=`echo $m |sed 's/[0-9]//g'`
-	echo $m
-done
