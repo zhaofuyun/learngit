25c70dcbc295d77c40532f6d05d89fe38cf5d46a
ken <1751728214@qq.com>
2016-02-29 12:57:24 +0800

    conflict fixed

--- case.sh
+++ case.sh
@@ -2,6 +2,7 @@
 #用case判断输入的数是奇数还是偶数
      read -p 'Please input a number: ' n
      m=$[$n%2]
+echo $m;
      case $m in
           1)
                echo '奇数'
---
+++ while.sh
@@ -0,0 +1,8 @@
+     #!/bin/bash
+#使用while每隔3秒输出时间
+     while :
+     do
+          date +%T
+          sleep 3
+     done
+
